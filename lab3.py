b0fb4fdb303c934e929db5eb183ee2b0003fda86
Maxim-Turovets <36541363+Maxim-Turovets@users.noreply.github.com>
2018-11-19 00:01:19 +0200

    Update lab3.py

--- lab3.py
+++ lab3.py
@@ -1,4 +1,29 @@
-string = "aa bb ff cc hh dd ee"
+#task 4
+
+string = "lorem ipsum sit amet consectetur adipisicing elit itaque omnis inventore dolor voluptas totam nobis"
 r=string.split(" ")
 r.sort()
 print(r)
+
+
+#task 5
+import math
+list_=[5,3,9,23,22,14,16,56,25,81,231,144,45,21,225]
+i=0
+
+
+def func():
+    global i
+    global list_
+    b=(math.sqrt(list_[i]))
+   
+    if (b.is_integer()==True):
+        print(list_[i])
+    i=i+1
+        
+    if(i<len(list_)):
+         return func()    
+  
+    
+func()
+  
